f832f5eee4b8f1a126bf783bd0331730eecce159
Phillip Harrington <pharrington@abre.com>
2026-04-10 21:01:40 -0400

    extracting from pdhc

---
+++ site.config.js
@@ -0,0 +1,8 @@
+/**
+ * Site-wide configuration. Injected into layouts via [[propName]] placeholders.
+ */
+export default {
+    siteUrl: '/',
+    siteName: 'My Site',
+    copyright: '&copy; My Site. All rights reserved.',
+};
