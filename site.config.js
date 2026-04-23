a2b6ab1f28a4e5eaf24d6d41a420a70ab60193fe
Phillip Harrington <phillip@phillipharrington.com>
2026-04-22 20:02:17 -0400

    updates skeleton for new module system

--- site.config.js
+++ site.config.js
@@ -1,8 +1,13 @@
 /**
  * Site-wide configuration. Injected into layouts via [[propName]] placeholders.
+ *
+ * `modules` is an optional array of module objects (from npm packages) or
+ * strings (local directory names under `modules/`). Modules can contribute
+ * pages and components to the build.
  */
 export default {
     siteUrl: '/',
     siteName: 'My Site',
     copyright: '&copy; My Site. All rights reserved.',
+    modules: [],
 };
